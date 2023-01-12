72e1cf122bf93575e284dff462cd744b5cc8806e
Jossean Rivera <32784144+jossean-rivera@users.noreply.github.com>
2023-01-12 20:07:15 +0000

    Starting challenge 07

---
+++ Challenge07/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int NumberOfEncodings(string s) 
+{
+    if (s.StartsWith('0')) 
+    {
+        return 0;
+    }
+    else if (s.Length <= 1)
+    {
+        return 1;
+    }
+
+    int total = 0;
+    if (int.Parse(s.Substring(0, 2)) <= 26) 
+    {
+        total += NumberOfEncodings(s.Substring(2));
+    }
+}
