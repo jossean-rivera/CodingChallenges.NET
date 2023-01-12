38a414e0ed0cf971dd4a9666cc7526b110dc5187
Jossean Rivera <32784144+jossean-rivera@users.noreply.github.com>
2023-01-12 19:24:58 +0000

    Finished Challenge 06

--- Challenge06/Program.cs
+++ Challenge06/Program.cs
@@ -1,99 +1,115 @@
-﻿
-var list = new ListXOR<int>();
+﻿using System.Runtime.InteropServices;
+
+using var list = new ListXOR<int>();
+
+Console.WriteLine("Adding items 1, 2, 3, 4, and 5");
 list.Add(1);
 list.Add(2);
 list.Add(3);
 list.Add(4);
+list.Add(5);
 
 Console.WriteLine("Item at index 2 is {0}", list.Get(2));
 Console.WriteLine("Done.");
 
-
-class ListXOR<T>
+class ListXOR<T> : IDisposable
 {
-    //  Hold a reference of the nodes to prevent garbage collection
-    private readonly List<Node<T>> _nodes = new();
-    private unsafe readonly List<ulong> _nodeIds = new();
+    //  Save the pinned handles so that we can free them once we're done with the list
+    private readonly List<GCHandle> _nodeHandles = new();
 
-    private Node<T> _head = null;
-    private Node<T> _tail = null;
+    //  .NET doesn't allow pointers of managed types of the GC.
+    //  We can get the address of the nodes but we can't access the nodes from the address.
+    //  So, we'll keep a dictionary of the node addresses as the keys, and the actual node as the value for the key.
+    private readonly Dictionary<nint, Node> _nodeIds = new();
+
+    private nint _headId;
+    private nint _tailId;
+
 
     public unsafe void Add(T value)
     {
         //  Create a new node to add
-        Node<T> node = new(value);
+        Node node = new(value);
 
-         //  Save the address of the node for debugging purposes only
-        ulong nodeId = (ulong)&node;
-        _nodeIds.Add(nodeId);
+        //  Save the address of the node
+        GCHandle handle = GCHandle.Alloc(node, GCHandleType.Pinned);
+        _nodeHandles.Add(handle);
+        nint nodeId = handle.AddrOfPinnedObject();
+        _nodeIds.Add(nodeId, node);
 
-       // Save the node to prevent the garbage collection of the new node
-        _nodes.Add(node);
 
-
-        if (_head is null)
+        if (_headId == default)
         {
-            _head = _tail = node;
+            _headId = _tailId = nodeId;
         }
         else
         {
-            void* nodePtr = &node;
-            _tail.both = (void*)((ulong)nodePtr ^ (ulong)_tail.both);
-            fixed (void* tailId = &_tail)
-            {
-                node.both = tailId;
-            }
-            _tail = node;
+            //  Get the node that the tails point to
+            Node tail = _nodeIds[_tailId];
+
+            //  Update the link of the tail node
+            tail.both = nodeId ^ tail.both;
+
+            //  Update the link of the new node
+            node.both = _tailId;
+
+            //  Set the tail to the new node
+            _tailId = nodeId;
         }
 
     }
 
     public unsafe T Get(int index)
     {
-        void* prevId = default(void*);
-        void* nextId = default(void*);
+        nint prevId = default;
+        nint nextId = default;
 
-        fixed (Node<T>* headPtr = &_head)
+        //  Create an iterator variable starting with head
+        nint nodeId = _headId;
+        Node node = null;
+
+        for (int i = 0; i <= index; i++)
         {
-            Node<T>* node = headPtr;
-            for (int i = 0; i < index; i++)
+            //  Get the node that the pointer is refrencing
+            node = _nodeIds[nodeId];
+
+            //  Move to the next node
+            nextId = prevId ^ node.both;
+
+            if (nextId != default)
             {
-                nextId = (void*)((ulong)prevId ^ (ulong)node->both);
-
-                if (nextId != default(void*))
-                {
-                    prevId = node;
-                    node = (Node<T>*)nextId;
-                }
-                else
-                {
-                    throw new IndexOutOfRangeException();
-                }
-
+                prevId = nodeId;
+                nodeId = nextId;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
             }
 
-            //  The node variable should be pointing to the node at the index
-            return node->value;
         }
 
-
-
-        return default(T);
+        //  The node variable should be pointing to the node at the index
+        return node.value;
     }
 
-    private class Node<T>
+    public void Dispose()
+    {
+        //  Release all the GC handles to unpinned the addresses
+        foreach (GCHandle handle in _nodeHandles)
+        {
+            handle.Free();
+        }
+    }
+
+    private class Node
     {
         public T value;
-        public unsafe void* both;
+        public nint both;
 
         public Node(T newValue)
         {
             value = newValue;
-
-            unsafe
-            {
-                both = default(void*);
-            }
+            both = default;
         }
     }
 }
--- Challenge06/README.md
+++ Challenge06/README.md
@@ -1,3 +1,5 @@
 # Challenge 06
 
 An XOR linked list is a more memory efficient doubly linked list. Instead of each node holding next and prev fields, it holds a field named both, which is an XOR of the next node and the previous node. Implement an XOR linked list; it has an add(element) which adds the element to the end, and a get(index) which returns the node at index.
+
+**Note:** Having a fully functional XOR list in .NET is not possible. The garbage collector will release from memory the nodes we don't have references to. We can "pin" the nodes and avoid the GC to collect them, but we still can't access the nodes from a pointer because .NET does not allow pointers of managed types. See [Pointer Types](https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/unsafe-code#pointer-types).
